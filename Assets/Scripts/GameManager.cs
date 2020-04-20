3c1ce3a403fc542ed0859626d552bc1e219310b5
David Neagu <neagudavid@gmail.com>
2020-04-20 19:16:07 +0300

    Cleanup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     private const float threshold = 3;
-    public enum State
-    {
+
+    public enum State {
         PickQuestion,
         WaitingForAnswer,
         ValidateAnswer,
@@ -19,7 +18,7 @@ public class GameManager : MonoBehaviour
         GiveVerdictOnAnswer,
         Null
     }
-    
+
     private object threadLocker = new object();
     private bool waitingForReco;
     private string currentInput = "ceva";
@@ -30,142 +29,126 @@ public class GameManager : MonoBehaviour
 
     private const string Fail = "You fail.";
     private const string Win = "You win.";
-    
-    private State _currentState = State.PickQuestion;
+
+    private State currentState = State.PickQuestion;
     private string currentAnswer;
-    private int IncorrectAnswersCount = 0;
+    private int incorrectAnswersCount = 0;
     private const int Lives = 3;
     public Light Light;
     private const int ColorFlashTime = 3;
-    private Color DefaultColor; 
+    private Color defaultColor;
 
-    public State currentState
-    {
-        get => _currentState;
-        set
-        {
-            switch (value)
-            {
+    public State CurrentState {
+        get => currentState;
+        set {
+            switch (value) {
                 case State.PickQuestion:
                     Debug.Log("pickQuestion");
-                    if (IncorrectAnswersCount == Lives)
-                    {
-                        EndGameMessage.gameObject.SetActive(true);
-                        EndGameMessage.color = Color.red;
-                        EndGameMessage.text = Fail;
+                    if (incorrectAnswersCount == Lives) {
+                        endGameMessage.gameObject.SetActive(true);
+                        endGameMessage.color = Color.red;
+                        endGameMessage.text = Fail;
                         // _currentState = State.Null;
                     }
-                    else if (currentQuestion == getQuestions.Questions.Count)
-                    {
-                        EndGameMessage.gameObject.SetActive(true);
-                        EndGameMessage.text = Win;
+                    else if (currentQuestion == getQuestions.Questions.Count) {
+                        endGameMessage.gameObject.SetActive(true);
+                        endGameMessage.text = Win;
                         // _currentState = State.Null;
                     }
-                    else
-                    {
-                        _currentState = value;
+                    else {
+                        currentState = value;
                     }
+
                     break;
                 case State.WaitingForAnswer:
                     Debug.Log("waitingforanswer");
                     waitingForReco = true;
                     GetInput();
-                    _currentState = value;
+                    currentState = value;
                     break;
                 case State.ValidateAnswer:
                     Debug.Log("validateAnswer");
                     Debug.Log(currentInput);
                     errorText.gameObject.SetActive(false);
-                    var closestAnswer= GetClosestAnswer(out var closestDistance);
+                    var closestAnswer = GetClosestAnswer(out var closestDistance);
                     currentAnswer = closestAnswer;
-                    if (closestDistance > threshold)
-                    {
+                    if (closestDistance > threshold) {
                         errorText.gameObject.SetActive(true);
                         errorText.text = UnintelligibleInput;
                         // StartCoroutine(WaitUserRead(State.WaitingForAnswer));
-                        currentState = State.WaitingForAnswer;
+                        CurrentState = State.WaitingForAnswer;
                     }
-                    else
-                    {
-                        for (var index = 0; index < answers.Count; index++)
-                        {
-                            if (answers[index].text == closestAnswer)
-                            {
+                    else {
+                        for (var index = 0; index < answers.Count; index++) {
+                            if (answers[index].text == closestAnswer) {
                                 answers[index].color = Color.yellow;
                                 Debug.Log(answers[index].text);
                                 // break;
                             }
-                            else
-                            {
+                            else {
                                 answers[index].color = Color.white;
                             }
                         }
 
-                        currentState = State.CheckForFinalAnswer;
+                        CurrentState = State.CheckForFinalAnswer;
                     }
+
                     break;
                 case State.CheckForFinalAnswer:
                     Debug.Log("check final");
                     errorText.gameObject.SetActive(false);
-                    FinalAnswerCheck.gameObject.SetActive(true);
+                    finalAnswerCheck.gameObject.SetActive(true);
                     waitingForReco = true;
                     GetInput();
-                    _currentState = value;
+                    currentState = value;
                     break;
                 case State.ValidateFinalAnswer:
                     Debug.Log("validate final answer");
-                    FinalAnswerCheck.gameObject.SetActive(false);
+                    finalAnswerCheck.gameObject.SetActive(false);
                     var finalAnswer = GetYesNo(out var minDistance);
-                    if (minDistance > 1)
-                    {
+                    if (minDistance > 1) {
                         errorText.text = UnintelligibleInput;
-                        currentState = State.CheckForFinalAnswer;
+                        CurrentState = State.CheckForFinalAnswer;
                         // StartCoroutine(WaitUserRead(State.CheckForFinalAnswer));
                     }
-                    else
-                    {
-                        if (finalAnswer == No)
-                        {
-                            currentState = State.WaitingForAnswer;
+                    else {
+                        if (finalAnswer == No) {
+                            CurrentState = State.WaitingForAnswer;
                         }
-                        else
-                        {
-                            currentState = State.GiveVerdictOnAnswer;
+                        else {
+                            CurrentState = State.GiveVerdictOnAnswer;
                         }
                     }
+
                     break;
                 case State.GiveVerdictOnAnswer:
                     Debug.Log("give verdict");
                     var correctAnswer = getQuestions.Questions[currentQuestion].correct_answer;
-                    int correctAnswerIndex=0, currentAnswerIndex=1;
-                    for (int i = 0; i < answers.Count; i++)
-                    {
-                        if (answers[i].text == correctAnswer)
-                        {
+                    int correctAnswerIndex = 0, currentAnswerIndex = 1;
+                    for (int i = 0; i < answers.Count; i++) {
+                        if (answers[i].text == correctAnswer) {
                             correctAnswerIndex = i;
                         }
-                        if (answers[i].text == currentAnswer)
-                        {
-                            currentAnswerIndex= i;
+
+                        if (answers[i].text == currentAnswer) {
+                            currentAnswerIndex = i;
                         }
                     }
+
                     answers[currentAnswerIndex].color = Color.red;
                     answers[correctAnswerIndex].color = Color.green;
                     currentQuestion++;
-                    if (correctAnswerIndex != currentAnswerIndex)
-                    {
-                        IncorrectAnswersCount++;
-                        LivesLeft.text = "Lives left: " + (Lives - IncorrectAnswersCount);
+                    if (correctAnswerIndex != currentAnswerIndex) {
+                        incorrectAnswersCount++;
+                        livesLeft.text = "Lives left: " + (Lives - incorrectAnswersCount);
                         StartCoroutine(ChangeLightColor(Color.red));
                     }
-                    else
-                    {
+                    else {
                         StartCoroutine(ChangeLightColor(Color.green));
-
                     }
 
-                    _currentState = value;
-                    currentState = State.Null;
+                    currentState = value;
+                    CurrentState = State.Null;
                     break;
                 case State.Null:
                     StartCoroutine(WaitUserRead(State.PickQuestion));
@@ -175,131 +158,98 @@ public class GameManager : MonoBehaviour
             }
         }
     }
-        
+
     private int currentQuestion = 0;
-    private float timeToWait = 2;
+    private const float TimeToWait = 2;
     public GetQuestions getQuestions;
 
-    
+
     public List<Text> answers;
     public Text errorText;
-    public Text FinalAnswerCheck;
-    public Text EndGameMessage;
+    public Text finalAnswerCheck;
+    public Text endGameMessage;
 
-    public Text LivesLeft;
-    // Start is called before the first frame update
-    void Start()
-    {
-        LivesLeft.text = "Lives left: " + Lives;
+    public Text livesLeft;
+
+    void Start() {
+        livesLeft.text = "Lives left: " + Lives;
         Utils.shuffle(getQuestions.Questions);
-        DefaultColor = Light.color;
+        defaultColor = Light.color;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(currentState);
-        switch (currentState)
-        {
+    void Update() {
+        switch (CurrentState) {
             case State.PickQuestion:
                 questionDisplay.DisplayQuestion(getQuestions.Questions[currentQuestion]);
-                currentState = State.WaitingForAnswer;
+                CurrentState = State.WaitingForAnswer;
                 break;
             case State.WaitingForAnswer:
-                // StartCoroutine(WaitForAnswer());
-                if (!waitingForReco)
-                {
-                    currentState = State.ValidateAnswer;
+                if (!waitingForReco) {
+                    CurrentState = State.ValidateAnswer;
                 }
+
                 break;
-            // case State.ValidateAnswer:
-            //     
-            //     break;
             case State.CheckForFinalAnswer:
-                if (!waitingForReco)
-                {
-                    currentState = State.ValidateFinalAnswer;
+                if (!waitingForReco) {
+                    CurrentState = State.ValidateFinalAnswer;
                 }
+
                 break;
-            // default:
-            //     throw new ArgumentOutOfRangeException();
         }
     }
 
-    
-    
-    private IEnumerator WaitUserRead(State nextState)
-    {
-        yield return new WaitForSeconds(timeToWait);
-        currentState = nextState;
+
+    private IEnumerator WaitUserRead(State nextState) {
+        yield return new WaitForSeconds(TimeToWait);
+        CurrentState = nextState;
     }
 
-    private IEnumerator ChangeLightColor(Color newColor)
-    {
+    private IEnumerator ChangeLightColor(Color newColor) {
         Light.color = newColor;
         yield return new WaitForSeconds(ColorFlashTime);
-        Light.color = DefaultColor;
+        Light.color = defaultColor;
     }
 
-    public async void GetInput()
-    {
-        // Creates an instance of a speech config with specified subscription key and service region.
-        // Replace with your own subscription key and service region (e.g., "westus").
+    private async void GetInput() {
         var config = SpeechConfig.FromSubscription("df65df3407f1482da419ff9469f74f6d", "westus");
 
-        // Make sure to dispose the recognizer after use!
-        using (var recognizer = new SpeechRecognizer(config))
-        {
-            lock (threadLocker)
-            {
+        using (var recognizer = new SpeechRecognizer(config)) {
+
+            lock (threadLocker) {
                 waitingForReco = true;
             }
 
-            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
-            // single utterance is determined by listening for silence at the end or until a maximum of 15
-            // seconds of audio is processed.  The task returns the recognition text as result.
-            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
-            // shot recognition like command or query.
-            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
-            var result = await recognizer.RecognizeOnceAsync();//.ConfigureAwait(false);
+            var result = await recognizer.RecognizeOnceAsync();
 
             // Checks result.
             string newMessage = string.Empty;
-            if (result.Reason == ResultReason.RecognizedSpeech)
-            {
+            if (result.Reason == ResultReason.RecognizedSpeech) {
                 newMessage = result.Text;
             }
-            else if (result.Reason == ResultReason.NoMatch)
-            {
+            else if (result.Reason == ResultReason.NoMatch) {
                 newMessage = "NOMATCH: Speech could not be recognized.";
             }
-            else if (result.Reason == ResultReason.Canceled)
-            {
+            else if (result.Reason == ResultReason.Canceled) {
                 var cancellation = CancellationDetails.FromResult(result);
                 newMessage = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
             }
 
-            lock (threadLocker)
-            {
+            lock (threadLocker) {
                 currentInput = newMessage;
                 waitingForReco = false;
-                Debug.Log("lock");
                 recognizer.Dispose();
             }
         }
     }
 
-    
-    private string GetClosestAnswer(out int closestDistance)
-    {
-        string correct = getQuestions.Questions[currentQuestion].correct_answer;
+
+    private string GetClosestAnswer(out int closestDistance) {
+        var correct = getQuestions.Questions[currentQuestion].correct_answer;
         closestDistance = Utils.LevenshteinDistance(currentInput, correct);
-        string closestAnswer = correct;
-        foreach (var answer in getQuestions.Questions[currentQuestion].incorrect_answers)
-        {
+        var closestAnswer = correct;
+        foreach (var answer in getQuestions.Questions[currentQuestion].incorrect_answers) {
             int distance = Utils.LevenshteinDistance(currentInput, answer);
-            if (distance < closestDistance)
-            {
+            if (distance < closestDistance) {
                 closestAnswer = answer;
                 closestDistance = distance;
             }
@@ -308,12 +258,10 @@ public class GameManager : MonoBehaviour
         return closestAnswer;
     }
 
-    private string GetYesNo(out int minDistance)
-    {
+    private string GetYesNo(out int minDistance) {
         var distanceYes = Utils.LevenshteinDistance(currentInput, Yes);
         var distanceNo = Utils.LevenshteinDistance(currentInput, No);
-        if (distanceYes > distanceNo)
-        {
+        if (distanceYes > distanceNo) {
             minDistance = distanceNo;
             return No;
         }
--- Assets/Scripts/GetQuestions.cs
+++ Assets/Scripts/GetQuestions.cs
@@ -4,41 +4,20 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class GetQuestions : MonoBehaviour
-{
+public class GetQuestions : MonoBehaviour {
     public List<Question> Questions;
-    public void LoadJson()
-    {
-        // using (StreamReader r = new StreamReader("Assets/questions.json"))
-        // {
-        string json = "{\"response_code\":0,\"results\":[{\"category\":\"Entertainment: Television\",\"type\":\"multiple\",\"difficulty\":\"easy\",\"question\":\"In the TV show &quot;Cheers&quot;, Sam Malone was a former relief pitcher for which baseball team?\",\"correct_answer\":\"Boston Red Sox\",\"incorrect_answers\":[\"New York Mets\",\"Baltimore Orioles\",\"Milwaukee Brewers\"]},{\"category\":\"Entertainment: Japanese Anime & Manga\",\"type\":\"multiple\",\"difficulty\":\"medium\",\"question\":\"In Dragon Ball Z, who was the first character to go Super Saiyan 2?\",\"correct_answer\":\"Gohan\",\"incorrect_answers\":[\"Goku\",\"Vegeta\",\"Trunks\"]},{\"category\":\"Science & Nature\",\"type\":\"multiple\",\"difficulty\":\"medium\",\"question\":\"To the nearest minute, how long does it take for light to travel from the Sun to the Earth?\",\"correct_answer\":\"8 Minutes\",\"incorrect_answers\":[\"6 Minutes\",\"2 Minutes\",\"12 Minutes\"]},{\"category\":\"Entertainment: Film\",\"type\":\"multiple\",\"difficulty\":\"medium\",\"question\":\"In Back to the Future Part II, Marty and Dr. Emmett Brown go to which future date?\",\"correct_answer\":\"October 21, 2015\",\"incorrect_answers\":[\"August 28, 2015\",\"July 20, 2015\",\"January 25, 2015\"]},{\"category\":\"Entertainment: Video Games\",\"type\":\"multiple\",\"difficulty\":\"easy\",\"question\":\"In which year was League of Legends released?\",\"correct_answer\":\"2009\",\"incorrect_answers\":[\"2010\",\"2003\",\"2001\"]}]}"; //r.ReadToEnd();
-            Questions = JsonUtility.FromJson<QuestionsClass>(json).results;
+
+    private void LoadJson() {
+        const string json = "{\"response_code\":0,\"results\":[{\"category\":\"Entertainment: Television\",\"type\":\"multiple\",\"difficulty\":\"easy\",\"question\":\"In the TV show &quot;Cheers&quot;, Sam Malone was a former relief pitcher for which baseball team?\",\"correct_answer\":\"Boston Red Sox\",\"incorrect_answers\":[\"New York Mets\",\"Baltimore Orioles\",\"Milwaukee Brewers\"]},{\"category\":\"Entertainment: Japanese Anime & Manga\",\"type\":\"multiple\",\"difficulty\":\"medium\",\"question\":\"In Dragon Ball Z, who was the first character to go Super Saiyan 2?\",\"correct_answer\":\"Gohan\",\"incorrect_answers\":[\"Goku\",\"Vegeta\",\"Trunks\"]},{\"category\":\"Science & Nature\",\"type\":\"multiple\",\"difficulty\":\"medium\",\"question\":\"To the nearest minute, how long does it take for light to travel from the Sun to the Earth?\",\"correct_answer\":\"8 Minutes\",\"incorrect_answers\":[\"6 Minutes\",\"2 Minutes\",\"12 Minutes\"]},{\"category\":\"Entertainment: Film\",\"type\":\"multiple\",\"difficulty\":\"medium\",\"question\":\"In Back to the Future Part II, Marty and Dr. Emmett Brown go to which future date?\",\"correct_answer\":\"October 21, 2015\",\"incorrect_answers\":[\"August 28, 2015\",\"July 20, 2015\",\"January 25, 2015\"]},{\"category\":\"Entertainment: Video Games\",\"type\":\"multiple\",\"difficulty\":\"easy\",\"question\":\"In which year was League of Legends released?\",\"correct_answer\":\"2009\",\"incorrect_answers\":[\"2010\",\"2003\",\"2001\"]}]}"; //r.ReadToEnd();
+        Questions = JsonUtility.FromJson<QuestionsClass>(json).results;
     }
-    
+
     [Serializable]
-    public class QuestionsClass
-    {
+    public class QuestionsClass {
         public List<Question> results;
     }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void Awake()
-    {
+    private void Awake() {
         LoadJson();
-        // foreach (var question in Questions)
-        // {
-        //     Debug.Log(question.question);
-        // }
     }
 }
